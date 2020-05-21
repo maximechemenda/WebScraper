ad00707e2dbf847fffa4235e237fb744cb7f43b1
Maxime Chemenda <maxime.chemenda@gmail.com>
2020-05-21 15:53:36 +0200

    made the first filter work

--- fileReader.py
+++ fileReader.py
@@ -8,16 +8,23 @@ def file_reader(file_name):
         for i in range(len(file_content)):
             if i > 4 and i != len(file_content) - 1: 
                 line = file_content[i]
-                temp_name = line[8:]
+                temp_name = line[7:]
                 temp_content.append(temp_name)
         
         final_content = []
         for i in range(len(temp_content)):
-            name = temp_content[i][:-2]
+            name = temp_content[i][:-1]
             final_content.append(name)
-        
-        print(final_content)
+
+        return final_content
+
+
+def delete_file_content(file_name):
+    file = open(file_name, "r+")
+    file.truncate(0)
+    file.close()
 
 
 if __name__ == '__main__':
-    file_reader("../testing.log")
+    file_reader("../content_chrome.log")
+    #delete_file_content("./infoo_test.csv")
--- mainInfo.py
+++ mainInfo.py
@@ -56,10 +56,57 @@ def get_data(url):
             for link in article.find_all('a'):
                 link = link.get_text()
                 row = ''.join(row + link) + ','
-            print([row]) #just for debugging
             test_writer.writerow([row])
 
 
+
+
+def update_data(values, parameter):
+    with open('infooo_test.csv', newline='') as csvfile:
+     spamreader = csv.reader(csvfile, delimiter='\n', quotechar='|')
+     file_content = []
+     for row in spamreader:
+        line_content = row[0]
+        file_content.append(line_content)
+    
+    result = []
+    for line in file_content:
+        splitted_line = line.split(',')
+        name = splitted_line[0]
+        for value in values:
+
+            if name == value:
+                print('working')
+                line = line + parameter  
+        result.append(line)
+
+    print(result)
+
+    
+#PROBABLY SHOULD GO IN A SEPARATE CLASS
+def file_reader(file_name):
+    with open(file_name) as file:
+        file_content = file.readlines()
+
+        temp_content = []
+        for i in range(len(file_content)):
+            if i > 4 and i != len(file_content) - 1: 
+                line = file_content[i]
+                temp_name = line[7:]
+                temp_content.append(temp_name)
+        
+        final_content = []
+        for i in range(len(temp_content)):
+            name = temp_content[i][:-1]
+            final_content.append(name)
+
+        return final_content
+
+
+
+
 if __name__ == '__main__':
-    get_data('https://www.polemermediterranee.com/Reseau/Annuaire-des-membres/(offset)/456')
+    #get_data('https://www.polemermediterranee.com/Reseau/Annuaire-des-membres/(offset)/456')
+    values = file_reader("../content_chrome.log")
+    update_data(values, 'Ile de France')
 
