61413e17b1ee106ae5209d95dc7e0c02f93fff88
Maxime Chemenda <maxime.chemenda@gmail.com>
2020-05-25 16:28:38 +0200

    done with regions

--- verificator.py
+++ verificator.py
@@ -1,10 +1,14 @@
 with open('pole_mer_data.txt', 'r+') as file:
     file_content = file.readlines()
-    counter = 0
+
+    for i,line in enumerate(file_content):
+        splitted_line = line.split('|||')
+        if len(splitted_line) != 5 and i != 0:
+            print(splitted_line)
+            file_content[i] = splitted_line[0] + '|||' + splitted_line[1] + '|||' + splitted_line[2] + '|||' + 'not provided' + '|||' + splitted_line[3]
+    file.seek(0)
 
     for line in file_content:
-        splitted_line = line.split('|||')
-        if len(splitted_line) != 4:
-            print(splitted_line)
-            counter += 1
-    print(counter)
+        file.write(line)
+
+          
