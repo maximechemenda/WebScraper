c41574ca45f2203640cf9b6a1824367b64896ef0
Maxime Chemenda <maxime.chemenda@gmail.com>
2020-05-26 10:33:43 +0200

    done with half of companies

--- verificator.py
+++ verificator.py
@@ -1,14 +1,27 @@
 with open('pole_mer_data.txt', 'r+') as file:
     file_content = file.readlines()
-
-    for i,line in enumerate(file_content):
+    counter = 0
+    other_one = 0
+    for line in file_content:
         splitted_line = line.split('|||')
-        if len(splitted_line) != 5 and i != 0:
+        if len(splitted_line) == 6:
+            counter += 1
+            if splitted_line[4] == 'not provided':
+                other_one += 1
+        
+    print(counter)
+    print(other_one)
+
+    
+
+    """ for i,line in enumerate(file_content):
+        splitted_line = line.split('|||')
+        if len(splitted_line) < 5 and i != 0:
             print(splitted_line)
-            file_content[i] = splitted_line[0] + '|||' + splitted_line[1] + '|||' + splitted_line[2] + '|||' + 'not provided' + '|||' + splitted_line[3]
+            #file_content[i] = splitted_line[0] + '|||' + splitted_line[1] + '|||' + splitted_line[2] + '|||' + 'not provided' + '|||' + splitted_line[3]
     file.seek(0)
 
     for line in file_content:
-        file.write(line)
+        file.write(line) """
 
           
